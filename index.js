b6d4ebfdfe76540a05eb2ca905ed27d60623c5fe
ebonyash15 <56212844+ebonyash15@users.noreply.github.com>
2019-10-06 18:12:09 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var a, b, n
+function add(a, b) {return (a + b)}
+function subtract(a, b) {return (a - b)}
+function multiply(a, b) {return (a * b)}
+function divide(a, b) {return (a / b)}
+function inc(n) {return (n+=1);}
+function dec(n) {return (n-=1);}
+function makeInt(n) {return(parseInt(n,10))}
+// it('parses n as an integer and returns the parsed integer', function() {expect(makeInt(a.toString())).toEqual(a)}
+// it('assumes base 10', function() {expect(makeInt('0x2328')).toEqual(0)})
+// it('returns NaN as appropriate', function() {expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)})
+function preserveDecimal(n) {return parseFloat(n.toString())}
